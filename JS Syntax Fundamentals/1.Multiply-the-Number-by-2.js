ff75d6614a774d04d10249afb22f5b5084429cb9
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-06-28 17:04:09 +0300

    JS Syntax

---
+++ JS Syntax Fundamentals/1.Multiply-the-Number-by-2.js
@@ -0,0 +1,3 @@
+function mulitply(num) {
+    return num * 2;
+}
---
+++ JS Syntax Fundamentals/3.Excellent-Grade.js
@@ -0,0 +1,8 @@
+function grade(grade) {
+    if (grade >= 5.50) {
+        return "Excellent"
+    }
+    else{
+        return "Not excellent"
+    }
+}
