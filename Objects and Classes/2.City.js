9d51336325cd18e3e7a3b7a552d21026133a250c
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-07-07 11:44:58 +0300

    JS Objects

---
+++ Objects and Classes/2.City.js
@@ -0,0 +1,14 @@
+function city(obj) {
+    
+    for (const key in obj) {
+        console.log(`${key} -> ${obj[key]}`)
+    }
+}
+
+city({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+    })
