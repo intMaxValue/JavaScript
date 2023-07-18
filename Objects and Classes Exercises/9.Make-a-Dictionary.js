00f430041da1b8bc5747e0f6902b58b002a4e8a2
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-07-18 16:39:48 +0300

    JS DOM

--- Objects and Classes Exercises/9.Make-a-Dictionary.js
+++ Objects and Classes Exercises/9.Make-a-Dictionary.js
@@ -1,30 +1,26 @@
-function combineStrings(strings) {
+function makeADictionary(arr) {
     let dictionary = {};
-  
-    // Parse and combine the strings into one object
-    for (let i = 0; i < strings.length; i++) {
-      const obj = JSON.parse(strings[i]);
-      const term = obj.term;
-      const definition = obj.description;
-      dictionary[term] = definition;
+    for (let el of arr) {
+        let curenrtEl = JSON.parse(el);
+        let key = Object.keys(curenrtEl)[0];
+        let value = Object.values(curenrtEl)[0];
+        dictionary[key] = value;
     }
-  
-    // Sort the dictionary alphabetically by terms
-    const sortedTerms = Object.keys(dictionary).sort();
-  
-    // Print each term and definition
-    sortedTerms.forEach((term) => {
-      const definition = dictionary[term];
-      console.log(`Term: ${term} => Definition: ${definition}`);
-    });
-  }
-  
-  // Example usage
-  const strings = [
-    '{"term":"apple","description":"A fruit that grows on trees."}',
-    '{"term":"banana","description":"A curved fruit with a yellow peel."}',
-    '{"term":"apple","description":"A company that makes computers and smartphones."}'
-  ];
-  
-  combineStrings(strings);
-  
+    Object.entries(dictionary).sort((a,b)=>a[0].localeCompare(b[0])).forEach(x => console.log(`Term: ${x[0]} => Definition: ${x[1]}`));
+    }
+
+    
+    makeADictionary([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
+    ]);
+    makeADictionary([
+    '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+    '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+    '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+    '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+    '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} ',
+    ]);
