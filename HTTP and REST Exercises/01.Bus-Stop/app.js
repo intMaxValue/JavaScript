91362f8074eaa2318ec99da6e7f7653583130ffe
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-07-29 12:55:25 +0300

    HTTP and REST

--- HTTP and REST Exercises/01.Bus-Stop/app.js
+++ HTTP and REST Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    // TODO:
+    const input = document.getElementById('stopId');
+    const baseURL = 'http://localhost:3030/jsonstore/bus/businfo/';
+    let stopName = document.getElementById('stopName');
+    let busesData = document.querySelector('#buses');
+    let nextStop = "";
+    busesData.innerHTML = "";
+
+    fetch(`${baseURL}${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+        stopName.textContent = data.name;
+        
+        let busesAndTimes = Object.entries(data.buses)
+        busesAndTimes.forEach(bus => {
+            let li = document.createElement('li');
+            let [busId, time] = bus;
+            li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+            busesData.appendChild(li);
+        })
+    })
+    .catch(err => {
+        stopName.textContent ='Error'
+    });
 }
