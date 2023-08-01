4d21dae41b49f681b821ddecbf6f5c393702506d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-08-01 15:45:32 +0300

    HTTP and REST

--- HTTP and REST Exercises/06.Phonebook/app.js
+++ HTTP and REST Exercises/06.Phonebook/app.js
@@ -1,5 +1,63 @@
 function attachEvents() {
-    // TODO:
+    const phoneBook = document.querySelector('#phonebook');
+    
+    const loadBtn = document.querySelector('#btnLoad');
+    const createBtn = document.querySelector('#btnCreate');
+    
+    const personInput = document.querySelector('#person');
+    const phoneInput = document.querySelector('#phone');
+
+    loadBtn.addEventListener('click', loadContacts);
+    createBtn.addEventListener('click', createContact);
+
+    async function loadContacts() {
+        phoneBook.innerHTML = '';
+
+        let res = await fetch('http://localhost:3030/jsonstore/phonebook/')
+        let phoneBookData = await res.json();
+        Object.values(phoneBookData).forEach(p => {
+            let li = document.createElement('li');
+            li.textContent = `${p.person}: ${p.phone}`
+            li.id = p._id;
+            
+            let deleteBtn = document.createElement('button');
+            deleteBtn.textContent = `Delete`
+            li.appendChild(deleteBtn);
+            
+            phoneBook.appendChild(li);
+
+            deleteBtn.addEventListener('click', deleteContact);
+
+            async function deleteContact(e) {
+                let parentLi = this.parentNode;
+                
+                await fetch (`http://localhost:3030/jsonstore/phonebook/${this.parentElement.id}`, {
+                    method: 'DELETE',
+                })
+                parentLi.remove();
+            }
+        })
+    }
+
+    async function createContact(){
+        let person = personInput.value;
+        let phone = phoneInput.value;
+
+        let postRequest = {
+            person: person,
+            phone: phone
+        }
+
+        await fetch (`http://localhost:3030/jsonstore/phonebook`, {
+            method: 'POST',
+            body: JSON.stringify(postRequest)
+        });
+
+        personInput.value = "";
+        phoneInput.value = "";
+
+        await loadContacts();
+    }
 }
 
 attachEvents();
--- HTTP and REST Exercises/07.Students/app.js
+++ HTTP and REST Exercises/07.Students/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-  // TODO:
+  const BASE_URL = 'http://localhost:3030/jsonstore/collections/students/';
+
+  const tableBody = document.querySelector('tbody');
+
+  const firstName = document.querySelector('input[name="firstName"]');
+  const lastName = document.querySelector('input[name="lastName"]');
+  const facultyNumber = document.querySelector('input[name="facultyNumber"]');
+  const grade = document.querySelector('input[name="grade"]');
+
+  const submitBtn = document.getElementById('submit');
+  document.addEventListener('DOMContentLoaded', getTable);
+  submitBtn.addEventListener('click', submit);
+
+  async function submit() {
+    if (!firstName.value || !lastName.value || !facultyNumber.value || !grade.value) {
+      return;
+    }
+      
+    let body = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        facultyNumber: facultyNumber.value,
+        grade: grade.value
+      }
+  
+      await fetch(BASE_URL, {
+        method: 'POST',
+        body: JSON.stringify(body)
+      }) 
+      
+      firstName.value = "";
+      lastName.value = "";
+      facultyNumber.value = "";
+      grade.value = "";
+
+      await getTable();
+    }
+
+  async function getTable() {
+    let res = await fetch(BASE_URL);
+    let students = await res.json();
+
+    tableBody.innerHTML = "";
+
+    for (const studentId in students) {
+      const student = students[studentId];
+      const row = tableBody.insertRow();
+      row.insertCell().textContent = student.firstName;
+      row.insertCell().textContent = student.lastName;
+      row.insertCell().textContent = student.facultyNumber;
+      row.insertCell().textContent = student.grade;
+    }
+  }
 }
 
 attachEvents();
